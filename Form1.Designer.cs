b576dbf5498650ec8f9eb942e551b9e4a79d0d50
sparkcolazet <sparkcolazet@gmail.com>
2020-10-14 20:59:55 +0300

    done suka

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -37,6 +37,15 @@
 			this.textBox4 = new System.Windows.Forms.TextBox();
 			this.label3 = new System.Windows.Forms.Label();
 			this.label4 = new System.Windows.Forms.Label();
+			this.textBox5 = new System.Windows.Forms.TextBox();
+			this.textBox6 = new System.Windows.Forms.TextBox();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			this.textBox7 = new System.Windows.Forms.TextBox();
+			this.textBox8 = new System.Windows.Forms.TextBox();
+			this.label7 = new System.Windows.Forms.Label();
+			this.label8 = new System.Windows.Forms.Label();
+			this.button2 = new System.Windows.Forms.Button();
 			this.SuspendLayout();
 			// 
 			// textBox1
@@ -114,11 +123,94 @@
 			this.label4.TabIndex = 8;
 			this.label4.Text = "Y2";
 			// 
+			// textBox5
+			// 
+			this.textBox5.Location = new System.Drawing.Point(571, 225);
+			this.textBox5.Name = "textBox5";
+			this.textBox5.Size = new System.Drawing.Size(100, 20);
+			this.textBox5.TabIndex = 9;
+			// 
+			// textBox6
+			// 
+			this.textBox6.Location = new System.Drawing.Point(688, 224);
+			this.textBox6.Name = "textBox6";
+			this.textBox6.Size = new System.Drawing.Size(100, 20);
+			this.textBox6.TabIndex = 10;
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(615, 206);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(14, 13);
+			this.label5.TabIndex = 11;
+			this.label5.Text = "X";
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Location = new System.Drawing.Point(741, 206);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(14, 13);
+			this.label6.TabIndex = 12;
+			this.label6.Text = "Y";
+			// 
+			// textBox7
+			// 
+			this.textBox7.Location = new System.Drawing.Point(571, 283);
+			this.textBox7.Name = "textBox7";
+			this.textBox7.Size = new System.Drawing.Size(100, 20);
+			this.textBox7.TabIndex = 13;
+			// 
+			// textBox8
+			// 
+			this.textBox8.Location = new System.Drawing.Point(688, 283);
+			this.textBox8.Name = "textBox8";
+			this.textBox8.Size = new System.Drawing.Size(100, 20);
+			this.textBox8.TabIndex = 14;
+			// 
+			// label7
+			// 
+			this.label7.AutoSize = true;
+			this.label7.Location = new System.Drawing.Point(615, 264);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(22, 13);
+			this.label7.TabIndex = 15;
+			this.label7.Text = "RX";
+			// 
+			// label8
+			// 
+			this.label8.AutoSize = true;
+			this.label8.Location = new System.Drawing.Point(738, 264);
+			this.label8.Name = "label8";
+			this.label8.Size = new System.Drawing.Size(22, 13);
+			this.label8.TabIndex = 16;
+			this.label8.Text = "RY";
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(571, 335);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(217, 60);
+			this.button2.TabIndex = 17;
+			this.button2.Text = "DRAW ELLIPSE";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.label8);
+			this.Controls.Add(this.label7);
+			this.Controls.Add(this.textBox8);
+			this.Controls.Add(this.textBox7);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.textBox6);
+			this.Controls.Add(this.textBox5);
 			this.Controls.Add(this.label4);
 			this.Controls.Add(this.label3);
 			this.Controls.Add(this.textBox4);
@@ -148,6 +240,15 @@
 		private System.Windows.Forms.TextBox textBox4;
 		private System.Windows.Forms.Label label3;
 		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.TextBox textBox5;
+		private System.Windows.Forms.TextBox textBox6;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.TextBox textBox7;
+		private System.Windows.Forms.TextBox textBox8;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.Label label8;
+		private System.Windows.Forms.Button button2;
 	}
 }
 
--- Form1.cs
+++ Form1.cs
@@ -64,7 +64,93 @@ namespace KG_task2
             }
         }
 
-        
+
+        public void BresenhamEllipse(int xc, int yc, int rx, int ry)
+        {
+            //Grid grid = new Grid();
+            double dx, dy, d1, d2;
+            int x, y;
+            x = 0;
+            y = ry;
+
+            // Initial decision parameter of region 1 
+            d1 = (ry * ry) - (rx * rx * ry) + (0.25 * rx * rx);
+            dx = 2 * ry * ry * x;
+            dy = 2 * rx * rx * y;
+
+            // For region 1 
+            while (dx < dy)
+            {
+
+                // Print points based on 4-way symmetry 
+                /*grid.DrawPixel(brush, x + xc, y + yc, graphics);
+                grid.DrawPixel(brush, -x + xc, y + yc, graphics);
+                grid.DrawPixel(brush, x + xc, -y + yc, graphics);
+                grid.DrawPixel(brush, -x + xc, -y + yc, graphics);*/
+
+                DrawBigPixel(x + xc, y + yc);
+                DrawBigPixel(-x + xc, y + yc);
+                DrawBigPixel(x + xc, -y + yc);
+                DrawBigPixel(-x + xc, -y + yc);
+
+                // Checking and updating value of 
+                // decision parameter based on algorithm 
+                if (d1 < 0)
+                {
+                    x++;
+                    dx = dx + (2 * ry * ry);
+                    d1 = d1 + dx + (ry * ry);
+                }
+                else
+                {
+                    x++;
+                    y--;
+                    dx = dx + (2 * ry * ry);
+                    dy = dy - (2 * rx * rx);
+                    d1 = d1 + dx - dy + (ry * ry);
+                }
+            }
+
+            // Decision parameter of region 2 
+            d2 = ((ry * ry) * ((x + 0.5) * (x + 0.5))) +
+                 ((rx * rx) * ((y - 1) * (y - 1))) -
+                  (rx * rx * ry * ry);
+
+            // Plotting points of region 2 
+            while (y >= 0)
+            {
+
+                // Print points based on 4-way symmetry 
+                /*grid.DrawPixel(brush, x + xc, y + yc, graphics);
+                grid.DrawPixel(brush, -x + xc, y + yc, graphics);
+                grid.DrawPixel(brush, x + xc, -y + yc, graphics);
+                grid.DrawPixel(brush, -x + xc, -y + yc, graphics);*/
+
+                DrawBigPixel(x + xc, y + yc);
+                DrawBigPixel(-x + xc, y + yc);
+                DrawBigPixel(x + xc, -y + yc);
+                DrawBigPixel(-x + xc, -y + yc);
+
+                // Checking and updating parameter 
+                // value based on algorithm 
+                if (d2 > 0)
+                {
+                    y--;
+                    dy = dy - (2 * rx * rx);
+                    d2 = d2 + (rx * rx) - dy;
+                }
+                else
+                {
+                    y--;
+                    x++;
+                    dx = dx + (2 * ry * ry);
+                    dy = dy - (2 * rx * rx);
+                    d2 = d2 + dx - dy + (rx * rx);
+                }
+            }
+        }
+
+
 
         private void DrawGrid(int numOfCells, int cellSize)
 		{
@@ -114,5 +200,15 @@ namespace KG_task2
 
 
         }
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+            int x = int.Parse(textBox5.Text);
+            int y = int.Parse(textBox6.Text);
+            int rx = int.Parse(textBox7.Text);
+            int ry = int.Parse(textBox8.Text);
+
+            BresenhamEllipse(x, y, rx, ry);
+		}
 	}
 }
